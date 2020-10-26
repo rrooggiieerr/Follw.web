108947d99d00b4d659429c3f63994a4a5d635f28
Rogier van Staveren <rogier@batoid.com>
2020-10-26 12:54:14 +0100

    Content updates and new bootstrap based design

---
+++ application/views/footer.php
@@ -0,0 +1,27 @@
+				<footer class="pt-4 my-md-5 pt-md-5 border-top">
+					<div class="row">
+						<div class="col">
+							<h5>Privacy</h5>
+							<ul class="list-unstyled text-small">
+								<li><a class="text-muted" href="/privacy">Privacy statement</a></li>
+								<li><a class="text-muted" href="/terms">Terms & conditions</a></li>
+							</ul>
+						</div>
+						<div class="col">
+							<h5>Apps</h5>
+							<ul class="list-unstyled text-small">
+								<li><a class="text-muted" href="https://play.google.com/store/apps/details?id=app.follw.share" target="_blank">Follw for Android</a></li>
+								<li><a class="text-muted" href="#" target="_blank">Follw for iOS</a></li>
+								<li><a class="text-muted" href="/osmand">Configure OsmAnd</a></li>
+							</ul>
+						</div>
+						<div class="col">
+							<h5>Integration</h5>
+							<ul class="list-unstyled text-small">
+								<li><a class="text-muted" href="/apidoc">API documentation</a></li>
+								<li><a class="text-muted" href="/wordpress">WordPress plugin</a></li>
+								<li><a class="text-muted" href="/htmlsnippet">HTML snippet</a></li>
+							</ul>
+						</div>
+					</div>
+				</footer>
--- application/views/intro.php
+++ application/views/intro.php
@@ -1,35 +1,86 @@
 <!doctype html>
 <html lang="en">
 	<head>
-		<title>Follw - Sharing your location with privacy</title>
+		<title>Follw · Sharing your location with privacy</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+<?php // Icons
+/* TODO
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180">
+		<link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png">
+		<link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png">
+		<link rel="icon" href="/favicon.ico">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#563d7c">
+		<meta name="msapplication-config" content="/browserconfig.xml">
+		<meta name="theme-color" content="#563d7c">
+*/
+?>
+<?php // Styles ?>
+		<link rel="stylesheet" href="https://unpkg.com/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+			integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+			crossorigin="anonymous"/>
+		<style>
+		</style>
 	</head>
 	<body>
-		<h1>Follw</h1>
-		<h2>Sharing your location with privacy</h2>
-		<h3>Introduction</h3>
-		<p>Follw is a privacy focused location sharing service. Only a unique Sharing ID and derived Sharing URL is
-		given and no account details, user credentials, IP addresses, Cookies and other sensitive information are used
-		or stored on the Follw servers.</p>
-		<p>Whenever a new location is submitted the previous location is overwritten, no location history is
-		stored.</p>
-		<p>If you decide to delete your unique Sharing ID all location details are removed from the Follw.app servers.
-		Only a hash of your Sharing ID is stored after removal to guarantee a Sharing ID is not reassigned again,
-		everything else is removed from the Follw servers.</p>
-		<!-- p><a href="#">Read the full Privacy statement here</a></p -->
-		<h3>Features</h3>
-		<p>Privacy focused, Follw doesn't store any credentials other than your Sharing ID, location and configuration
-		parameters.</p>
-		<p>Add free, the Follw bussiness model is not based on advertisments and is free for personal and low volume
-		use.</p>
-		<p>Share any location, it doesn't have to be where you currently are.</p>
-		<p>Control who can follow you when, prospone or retract following permission anytime and limit the follow
-		duration.</p>
-		<p>Documented API, easy integration with mapping software, tracking hardware and your custom software.</p>
-		<p>Open Source, you can validate the security and workings of the software, copy it, modify it to your own
-		needs and host it yourself. Find the Follw WebApp and API source code on
-		<a href="https://github.com/rrooggiieerr/Follw.web" target="_blank">GitHub</a></p>
-		<h3>Getting Started</h3>
-		<p>Start sharing your location by generating a Sharing ID</p>
-		<a href="/generatesharingid">Start sharing your location</a>
+		<main>
+			<div class="jumbotron">
+				<div class="container">
+					<h1>Follw <small class="h4 text-muted">· Sharing your location with privacy</small></h1>
+					<p>Follw is a privacy focused location sharing service. Only a unique Sharing ID is assigned and
+					no user credentials, cookies and other sensitive information are stored on the Follw servers.</p>
+					<p>Whenever a location is expired it is deleted, no unnecessary location history is stored. If you
+					decide to delete your Sharing ID all location details are removed from the Follw servers.</p>
+					<p>Start sharing any location, it doesn't have to be where you currently are, by generating a Sharing ID.
+					Configure who can follow you when, prospone or retract following permission anytime and limit the follow
+					duration. You are in control.</p>
+					<p><a class="btn btn-primary btn-lg" href="/generatesharingid" role="button">Start sharing any location &raquo;</a></p>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg">
+						<div class="row">
+							<div class="col-md">
+								<h2>Privacy focused</h2>
+								<p>Follw doesn't store any credentials other than your Sharing ID, location and configuration
+								parameters.</p>
+								<p><a class="btn btn-secondary" href="#" role="button">Read the Privacy Statement &raquo;</a></p>
+							</div>
+							<div class="col-md">
+								<h2>Add free</h2>
+								<p>The Follw bussiness model is not based on advertisments and is free for personal and low volume
+								use.</p>
+								<p><a class="btn btn-secondary" href="#" role="button">Read more &raquo;</a></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg">
+						<div class="row">
+							<div class="col-md">
+								<h2>Documented API</h2>
+								<p>Easy integration with mapping software, tracking hardware and your custom software.</p>
+								<p><a class="btn btn-secondary" href="/api" role="button" target="_blank">Read the documentation &raquo;</a></p>
+							</div>
+							<div class="col-md">
+								<h2>Open Source</h2>
+								<p>You can validate the security and workings of Follw, copy it, modify it to your own
+								needs and host it yourself.</p>
+								<p><a class="btn btn-secondary" href="https://github.com/rrooggiieerr/Follw.web" role="button" target="_blank">Get the sourcode on GitHub &raquo;</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+<?php include_once('footer.php');?>
+			</div>
+		</main>
+<?php	// Scripts
+		// Placed at the end of the document so the pages load faster ?>
+		<script src="https://unpkg.com/jquery@3.5.1/dist/jquery.min.js"
+			integrity="sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2"
+			crossorigin="anonymous"></script>
+		<script src="https://unpkg.com/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+			crossorigin="anonymous"></script>
 	</body>
 </html>
