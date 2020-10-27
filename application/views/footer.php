3c873a3b0911ab423f4cc14814642c7f1330a687
Rogier van Staveren <rogier@batoid.com>
2020-10-27 17:39:50 +0100

    Improve responsiveness

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,35 +1,43 @@
 				<footer class="pt-4 my-md-5 pt-md-5 border-top">
 					<div class="row">
-						<div class="col">
-							<h5>About</h5>
-							<ul class="list-unstyled text-small">
-								<li><a class="text-muted" href="/credits">Credits</a></li>
-								<li><a class="text-muted" href="https://www.instagram.com/follw.app/" target="_blank">Follw on Instagram</a></li>
-							</ul>
+						<div class="col-md">
+							<div class="row">
+								<div class="col">
+									<h5>About</h5>
+									<ul class="list-unstyled text-small">
+										<li><a class="text-muted" href="/credits">Credits</a></li>
+										<li><a class="text-muted" href="https://www.instagram.com/follw.app/" target="_blank">Follw on Instagram</a></li>
+									</ul>
+								</div>
+								<div class="col">
+									<h5>Privacy</h5>
+									<ul class="list-unstyled text-small">
+										<li><a class="text-muted" href="/privacy">Privacy statement</a></li>
+										<li><a class="text-muted" href="/terms">Terms & conditions</a></li>
+									</ul>
+								</div>
+							</div>
 						</div>
-						<div class="col">
-							<h5>Privacy</h5>
-							<ul class="list-unstyled text-small">
-								<li><a class="text-muted" href="/privacy">Privacy statement</a></li>
-								<li><a class="text-muted" href="/terms">Terms & conditions</a></li>
-							</ul>
-						</div>
-						<div class="col">
-							<h5>Apps</h5>
-							<ul class="list-unstyled text-small">
-								<li><a class="text-muted" href="https://play.google.com/store/apps/details?id=app.follw.share" target="_blank">Follw for Android</a></li>
-								<li><a class="text-muted" href="#" target="_blank">Follw for iOS</a></li>
-								<li><a class="text-muted" href="/osmand">Configure OsmAnd</a></li>
-							</ul>
-						</div>
-						<div class="col">
-							<h5>Integration</h5>
-							<ul class="list-unstyled text-small">
-								<li><a class="text-muted" href="/apidoc">API documentation</a></li>
-								<li><a class="text-muted" href="/wordpress">WordPress plugin</a></li>
-								<li><a class="text-muted" href="/htmlsnippet">HTML snippet</a></li>
-								<li><a class="text-muted" href="https://github.com/rrooggiieerr/Follw.py" target="_blank">Python client on GitHub</a></li>
-							</ul>
+						<div class="col-md">
+							<div class="row">
+								<div class="col">
+									<h5>Apps</h5>
+									<ul class="list-unstyled text-small">
+										<li><a class="text-muted" href="https://play.google.com/store/apps/details?id=app.follw.share" target="_blank">Follw for Android</a></li>
+										<li><a class="text-muted" href="#" target="_blank">Follw for iOS</a></li>
+										<li><a class="text-muted" href="/osmand">Configure OsmAnd</a></li>
+									</ul>
+								</div>
+								<div class="col">
+									<h5>Integration</h5>
+									<ul class="list-unstyled text-small">
+										<li><a class="text-muted" href="/apidoc">API documentation</a></li>
+										<li><a class="text-muted" href="/wordpress">WordPress plugin</a></li>
+										<li><a class="text-muted" href="/htmlsnippet">HTML snippet</a></li>
+										<li><a class="text-muted" href="https://github.com/rrooggiieerr/Follw.py" target="_blank">Python client on GitHub</a></li>
+									</ul>
+								</div>
+							</div>
 						</div>
 					</div>
 				</footer>
