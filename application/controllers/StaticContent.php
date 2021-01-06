3e88991cbc49b998221920e44d600c563d3ff456
Rogier van Staveren <rogier@batoid.com>
2021-01-06 13:01:00 +0100

    Moving to a MVC-ish implementation

---
+++ application/controllers/StaticContent.php
@@ -0,0 +1,27 @@
+<?php
+class StaticContent {
+	/**
+	 * Handles static content
+	 * @param string $path
+	 * @return boolean TRUE if succesfully handled else FALSE
+	 */
+	function route(string $path) {
+		switch($path) {
+			case '/':
+				require_once(dirname(__DIR__) . '/views/intro.php');
+				return TRUE;
+			case '/privacy':
+			case '/terms':
+			case '/htmlsnippet':
+			case '/wordpress':
+			case '/osmand':
+				require_once(dirname(__DIR__) . '/views/static.php');
+				return TRUE;
+			case '/robots.txt':
+				require_once(dirname(__DIR__) . '/views/robots.txt.php');
+				return TRUE;
+		}
+
+		return FALSE;
+	}
+}
