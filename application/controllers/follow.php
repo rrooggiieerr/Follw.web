a1abd4effc7571980ee47bcf196040bfe571edc1
Rogier van Staveren <rogier@batoid.com>
2021-01-07 14:06:51 +0100

    Refactoring and solve location on page load

--- application/controllers/follow.php
+++ application/controllers/follow.php
@@ -16,36 +16,40 @@ if($method === 'POST') {
 }
 
 if($action === 'location') {
-	if($format === 'html') {
-		require_once(dirname(__DIR__) . '/views/follow.php');
-		exit();
-	}
-	
-	if(!$id->enabled) {
+	if(!$id->enabled || ($id->expires > 0 && $id->expires < time())) {
+		// If Follow ID has not been enabled or has expired
+		// HTTP Response 403 Forbidden
 		http_response_code(403);
-		exit();
-	}
-
-	if($id->expires > 0 && $id->expires < time()) {
-		http_response_code(403);
+		if($format === 'html') {
+			$location = NULL;
+			require_once(dirname(__DIR__) . '/views/follow.php');
+		}
 		exit();
 	}
 
 	require_once(dirname(__DIR__) . '/models/Location.php');
 	$location = Location::get($id);
 
+	if($format === 'html') {
+		require_once(dirname(__DIR__) . '/views/follow.php');
+		exit();
+	}
+
+	if (!$location) {
+		// If no location has been set
+		// HTTP Response 204 No Content
+		http_response_code(204);
+		exit();
+	}
+
 	switch ($format) {
 		case 'json':
-			if (!$location) {
-				http_response_code(204);
-				exit();
-			}
 			header('Content-Type: application/json');
 			$json = json_encode(array_merge($location->jsonSerialize(), $id->jsonSerialize()), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 			header('Content-Length: ' . strlen($json));
 			echo($json);
 			break;
-			//TODO Implement other formats
+		//TODO Implement other formats
 		default:
 			http_response_code(404);
 	}
