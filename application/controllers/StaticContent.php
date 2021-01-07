35cd1e5aa581e76594a285f9516b0a28873fa493
Rogier van Staveren <rogier@batoid.com>
2021-01-07 16:43:10 +0100

    Handle more static files

--- application/controllers/StaticContent.php
+++ application/controllers/StaticContent.php
@@ -6,6 +6,7 @@ class StaticContent {
 	 * @return boolean TRUE if succesfully handled else FALSE
 	 */
 	function route(string $path) {
+		$matches = NULL; // Fixes false "Variable is undefined" validation error
 		switch($path) {
 			case '/':
 				require_once(dirname(__DIR__) . '/views/intro.php');
@@ -20,10 +21,21 @@ class StaticContent {
 			case '/robots.txt':
 				require_once(dirname(__DIR__) . '/views/robots.txt.php');
 				return TRUE;
-			case '/follw.js':
-				header('Content-Type: text/javascript');
-				print(file_get_contents(dirname(__DIR__) . '/../htdocs/follw.js'));
-				return TRUE;
+			case (preg_match('/^\/[a-zA-Z0-9]*\.(html|js|css)$/', $path, $matches) ? true : false):
+				$filename = dirname(__DIR__) . '/../htdocs' . $path;
+				if(file_exists($filename)) {
+					switch($matches[1]) {
+						case 'js':
+							header('Content-Type: text/javascript');
+							break;
+						case 'css':
+							header('Content-Type: text/css');
+							break;
+					}
+					print(file_get_contents($filename));
+					return TRUE;
+				}
+				return FALSE;
 		}
 
 		return FALSE;
