d699863b020fdf5bfa93a5c0e128d4ab5511ddab
rogier <rogier@localhost>
2021-02-10 20:50:37 +0100

    Remove handling certain static files

--- application/controllers/StaticContent.php
+++ application/controllers/StaticContent.php
@@ -21,27 +21,6 @@ class StaticContent {
 			case '/robots.txt':
 				require_once(dirname(__DIR__) . '/views/robots.txt.php');
 				return TRUE;
-			case (preg_match('/^\/[a-zA-Z0-9_]*\.(html|js|css|png|svg)$/', $path, $matches) ? true : false):
-				$filename = dirname(__DIR__) . '/../htdocs' . $path;
-				if(file_exists($filename)) {
-					switch($matches[1]) {
-						case 'js':
-							header('Content-Type: text/javascript');
-							break;
-						case 'css':
-							header('Content-Type: text/css');
-							break;
-						case 'png':
-							header('Content-Type: image/png');
-							break;
-						case 'svg':
-							header('Content-Type: image/svg+xml');
-							break;
-					}
-					print(file_get_contents($filename));
-					return TRUE;
-				}
-				return FALSE;
 		}
 
 		return FALSE;
