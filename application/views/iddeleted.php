7c6d74d7ef3fd9b3663842cc08ef86b5b32ba8c5
Rogier van Staveren <rogier@batoid.com>
2021-02-16 14:26:52 +0100

    Delete Service Worker when ID has been deleted

--- application/views/iddeleted.php
+++ application/views/iddeleted.php
@@ -1,3 +1,7 @@
+<?php
+// Fixes false "Variable is undefined" validation errors
+/* @var ID $id */
+?>
 <html>
 	<head>
 		<title>ID has been deleted</title>
@@ -5,4 +9,16 @@
 	<body>
 		ID has been deleted
 	</body>
+	<script>
+		if(window.navigator && navigator.serviceWorker) {
+			navigator.serviceWorker.getRegistration("/<?= $id->encode() ?>/").then(function(registration) {
+				if(registration){
+					console.debug("Unregistering Service Worker");
+					registration.unregister()
+				} else {
+					console.debug("No Service Worker to unregister");
+				}
+			});
+		}
+	</script>
 </html>
