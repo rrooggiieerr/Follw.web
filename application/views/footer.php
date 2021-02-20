9168b98541d8662f8cb75e44a9f00c80fa80f396
Rogier van Staveren <rogier@batoid.com>
2021-02-20 12:15:24 +0100

    Add Twitter handle and update Instagram handle

--- application/views/footer.php
+++ application/views/footer.php
@@ -9,8 +9,9 @@ global $configuration;
 									<h5>About</h5>
 									<ul class="list-unstyled text-small">
 										<li><a class="text-muted" href="https://blog.follw.app/" target="_blank" rel="noopener noreferrer">Blog</a></li>
-										<li><a class="text-muted" href="https://www.instagram.com/follw.app/" target="_blank" rel="noopener noreferrer">Follw on Instagram</a></li>
+										<li><a class="text-muted" href="https://www.instagram.com/follwapp/" target="_blank" rel="noopener noreferrer">Follw on Instagram</a></li>
 										<li><a class="text-muted" href="https://www.facebook.com/follwapp" target="_blank" rel="noopener noreferrer">Follw on Facebook</a></li>
+										<li><a class="text-muted" href="https://twitter.com/follw_app" target="_blank" rel="noopener noreferrer">Follw on Twitter</a></li>
 									</ul>
 								</div>
 								<div class="col">
