26c67be6e81756d432034bd739d2dd2d639f380b
Rogier van Staveren <rogier@batoid.com>
2021-05-21 13:37:05 +0200

    Improved contact form

--- application/controllers/StaticContent.php
+++ application/controllers/StaticContent.php
@@ -6,7 +6,15 @@ class StaticContent {
 	 * @return boolean TRUE if succesfully handled else FALSE
 	 */
 	function route(string $path) {
-		global $configuration;
+		global $configuration, $method;
+		
+		if($path === '/contact') {
+			return $this->contact();
+		}
+
+		if($method === 'POST') {
+			return FALSE;
+		}
 
 		switch($path) {
 			case '/':
@@ -17,7 +25,6 @@ class StaticContent {
 			case '/credits':
 			case '/privacy':
 			case '/terms':
-			case '/contact':
 			case '/htmlsnippet':
 			case '/wordpress':
 				$filename = dirname(__DIR__) . '/views' . $path. '.html';
@@ -69,5 +76,162 @@ class StaticContent {
 		}
 
 		return FALSE;
+	}
+
+	function contact() {
+		global $configuration, $method;
+
+		if(empty($configuration['contactemail'])) {
+			http_response_code(404);
+			exit();
+		}
+
+		require_once(dirname(__DIR__) . '/models/Translation.php');
+		$tl = new Translation('contact');
+		header('Content-Language: ' . $tl->language);
+		
+		$errors = [];
+		$name = '';
+		$email = '';
+		$subject = '';
+		$message = '';
+		
+		if($method === 'POST') {
+			if(empty($_POST['sessionid'])) {
+				error_log('No session ID');
+				return FALSE;
+			}
+			
+			require_once(dirname(__DIR__) . '/libs/FormHoneypot.php');
+			require_once(dirname(__DIR__) . '/libs/Obfuscator.php');
+			require_once(dirname(__DIR__) . '/libs/TextCaptcha.php');
+			$sessionid = $_POST['sessionid'];
+			session_id($sessionid);
+			session_start();
+			
+			if(empty($_SESSION['honeypot'])) {
+				error_log('No honeypot in session');
+				return FALSE;
+			}
+			
+			$honeypot = $_SESSION['honeypot'];
+			$honeypot->validate($_POST);
+			
+			$obfuscator = $_SESSION['obfuscator'];
+			
+			if($configuration['captcha']['enabled']) {
+				if(empty($_SESSION['captcha'])) {
+					error_log('No captcha in session');
+					return FALSE;
+				}
+				
+				$captcha = $_SESSION['captcha'];
+				
+				if(empty($_POST[$obfuscator['captchaanswer']])) {
+					error_log('Empty captcha answer');
+					return FALSE;
+				}
+				
+				$answer = $_POST[$obfuscator['captchaanswer']];
+				
+				if(!$captcha->validate($answer)) {
+					error_log('Captcha answer invalid');
+					// The request is not validated to be performed by a human, try again
+					return FALSE;
+				}
+				// The request is validated to be performed by a human, continue sending the message
+			}
+
+			if(!empty($_POST['name'])) {
+				$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+			}
+				
+			if(!empty($_POST['email'])) {
+				$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+				if(!$email) {
+					$errors['email'] = 'errorinvalidemail';
+					$email = $_POST['email'];
+				}
+			} else {
+				$errors['email'] = 'errornoemail';
+			}
+				
+			$subject = '';
+			if(!empty($_POST['subject'])) {
+				$subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+			}
+				
+			if(isset($_POST['message']) && !empty($_POST['message'])) {
+				$message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+			} else {
+				$errors['message'] = 'errornomessage';
+			}
+				
+			if(!$errors) {
+				$headers = [];
+				
+				$headers['MIME-Version'] = '1.0';
+				$headers['Content-type'] = 'text/plain; charset=utf-8';
+				if(!empty($name)) {
+					$headers['From'] = $name . ' <' . $email . '>';
+				} else {
+					$headers['From'] = $email;
+				}
+				
+				if(mail($configuration['contactemail'], $subject, $message, $headers)) {
+?>
+<p>Thank you</p>
+<?php
+				} else {
+?>
+<p>Failed to send message</p>
+<?php
+				}
+
+				return TRUE;
+			}
+		}
+
+		if($method === 'GET' || !empty($errors)) {
+			session_start(['gc_maxlifetime' => 300]);
+			
+			require_once(dirname(__DIR__) . '/libs/FormHoneypot.php');
+			$honeypot = new FormHoneypot();
+			$_SESSION['honeypot'] = $honeypot;
+			require_once(dirname(__DIR__) . '/libs/Obfuscator.php');
+			$obfuscator = new Obfuscator();
+			$_SESSION['obfuscator'] = $obfuscator;
+			
+			//TODO Maybe only show captcha if client IP address is listed in spam list?
+			$captcha = NULL;
+			if($configuration['captcha']['enabled']) {
+				require_once(dirname(__DIR__) . '/libs/TextCaptcha.php');
+				$captcha = TextCaptcha::getCaptcha();
+				$_SESSION['captcha'] = $captcha;
+			}
+			
+			$filename = dirname(__DIR__) . '/views/contact.php';
+			$evaluate = TRUE;
+
+			if(array_key_exists('raw', $_GET)) {
+				header('X-Robots-Tag: noindex');
+				$raw = TRUE;
+			}
+			
+			$lastmodified = filemtime($filename);
+			if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && date('D, d M Y H:i:s T', $lastmodified) === $_SERVER['HTTP_IF_MODIFIED_SINCE']) {
+				http_response_code(304);
+				return TRUE;
+			}
+			header('Last-Modified: ' . date('D, d M Y H:i:s T', $lastmodified));
+			
+			if(isset($raw)) {
+				include($filename);
+			} else {
+				require_once(dirname(__DIR__) . '/views/static.php');
+			}
+
+			return TRUE;
+		}
 	}
 }
--- application/views/contact.php
+++ application/views/contact.php
@@ -1,102 +1,44 @@
 <?php
 require_once(dirname(__DIR__) . '/models/Translation.php');
 
-global $configuration;
-
-if(empty($configuration['contactemail'])) {
-	ob_end_clean();
-	http_response_code(404);
-	exit();
-	
-}
-
-$tl = new Translation('contact');
-header('Content-Language: ' . $tl->language);
-
-$errors = [];
-$name = '';
-$email = '';
-$subject = '';
-$message = '';
-
-if($_SERVER['REQUEST_METHOD'] === 'POST' || count($_GET) !== 0) {
-	if($_SERVER['REQUEST_METHOD'] == 'POST') {
-		$formValues = $_POST;
-	} else {
-		$formValues = $_GET;
-	}
-
-	if(!empty($formValues['name'])) {
-		$name = filter_var($formValues['name'], FILTER_SANITIZE_STRING);
-	}
-
-	if(!empty($formValues['email'])) {
-		$email = filter_var($formValues['email'], FILTER_VALIDATE_EMAIL);
-		if(!$email) {
-			$errors['email'] = 'errorinvalidemail';
-			$email = $formValues['email'];
-		}
-	} else {
-		$errors['email'] = 'errornoemail';
-	}
-
-	$subject = '';
-	if(!empty($formValues['subject'])) {
-		$subject = filter_var($formValues['subject'], FILTER_SANITIZE_STRING);
-	}
-
-	if(isset($formValues['message']) && !empty($formValues['message'])) {
-		$message = filter_var($formValues['message'], FILTER_SANITIZE_STRING);
-	} else {
-		$errors['message'] = 'errornomessage';
-	}
-
-	if(!$errors) {
-		$headers = [];
-
-		$headers['MIME-Version'] = '1.0';
-		$headers['Content-type'] = 'text/plain; charset=utf-8';
-		if(!empty($name)) {
-			$headers['From'] = $name . ' <' . $email . '>';
-		} else {
-			$headers['From'] = $email;
-		}
-
-		if(mail($configuration['contactemail'], $subject, $message, $headers)) {
+// Fixes false "Variable is undefined" validation errors
+/* @var String $question */
+/* @var TextCaptcha $captcha */
+/* @var FormHoneypot $honeypot */
+/* @var Obfuscator $obfuscator */
 ?>
-<p>Thank you</p>
-<?php
-		} else {
-?>
-<p>Failed to send message</p>
-<?php
-		}
-	}
-}
-
-if(($_SERVER['REQUEST_METHOD'] === 'GET' && count($_GET) == 0) || !empty($errors)) {
-?>
-<p></p>
-<form action="/contact"  method="post">
+<p><?= $tl->get('contactintro', 'html') ?></p>
+<form action="<?= $_SERVER['REQUEST_URI'] ?>"  method="post">
+	<input type="hidden" name="sessionid" value="<?= session_id() ?>"/>
 	<div class="form-group">
 		<label for="name"><?= $tl->get('namelabel', 'html') ?></label>
-		<input type="text" class="form-control" id="name" name="name" placeholder="<?= $tl->get('nameplaceholder', 'htmlattr') ?>" value="<?= htmlspecialchars($name, ENT_COMPAT) ?>">
+		<input type="text" class="form-control" id="name" name="<?= $obfuscator->obfuscate('name') ?>" placeholder="<?= $tl->get('nameplaceholder', 'htmlattr') ?>" value="<?= htmlspecialchars($name, ENT_COMPAT) ?>">
 	</div>
 	<div class="form-group">
 		<label for="email"><?= $tl->get('emaillabel', 'html') ?></label>
 		<small id="emailerror" class="form-text text-danger"><?= isset($errors['email']) ? $tl->get($errors['email'], 'html') : '' ?></small>
-		<input type="email" class="form-control" id="email" name="email" aria-describedby="emailhelp" placeholder="<?= $tl->get('emailplaceholder', 'htmlattr') ?>" value="<?= htmlspecialchars($email, ENT_COMPAT) ?>" required>
+		<input type="email" class="form-control" id="email" name="<?= $obfuscator->obfuscate('email') ?>" aria-describedby="emailhelp" placeholder="<?= $tl->get('emailplaceholder', 'htmlattr') ?>" value="<?= htmlspecialchars($email, ENT_COMPAT) ?>" required>
 		<small id="emailhelp" class="form-text text-muted"><?= $tl->get('emailhelp', 'html') ?></small>
 	</div>
 	<div class="form-group">
 		<label for="subject"><?= $tl->get('subjectlabel', 'html') ?></label>
-		<input type="text" class="form-control" id="subject" name="subject" placeholder="<?= $tl->get('subjectplaceholder', 'htmlattr') ?>" value="<?= htmlspecialchars($subject, ENT_COMPAT) ?>">
+		<input type="text" class="form-control" id="subject" name="<?= $obfuscator->obfuscate('subject') ?>" placeholder="<?= $tl->get('subjectplaceholder', 'htmlattr') ?>" value="<?= htmlspecialchars($subject, ENT_COMPAT) ?>">
 	</div>
 	<div class="form-group">
-		<small id="emailerror" class="form-text text-danger"><?= isset($errors['message']) ? $tl->get($errors['message'], 'html') : '' ?></small>
+		<small id="messageerror" class="form-text text-danger"><?= isset($errors['message']) ? $tl->get($errors['message'], 'html') : '' ?></small>
 		<label for="message"><?= $tl->get('messagelabel', 'html') ?></label>
-		<textarea id="message" class="form-control" name="message" placeholder="<?= $tl->get('messageplaceholder', 'htmlattr') ?>" rows="15" required><?= htmlspecialchars($message, ENT_NOQUOTES) ?></textarea>
+		<textarea id="message" class="form-control" name="<?= $obfuscator->obfuscate('message') ?>" placeholder="<?= $tl->get('messageplaceholder', 'htmlattr') ?>" rows="15" required><?= htmlspecialchars($message, ENT_NOQUOTES) ?></textarea>
 	</div>
+<?php if($captcha !== NULL) { ?>
+	<div id="captcha" class="form-group">
+		<p><?= $tl->get('captchaintro', 'html') ?></p>
+		<p id="captchaquestion"><?= htmlspecialchars($captcha->question) ?></p>
+		<small id="messageerror" class="form-text text-danger"><?= isset($errors['captchaanswer']) ? $tl->get($errors['captchaanswer'], 'html') : '' ?></small>
+		<div id="captchaanswer" class="form-group">
+			<label for="captchaanswer"><?= $tl->get('youranswer', 'html') ?></label>
+			<input type="text" id="captchaanswer" class="form-control" name="<?= $obfuscator->obfuscate('captchaanswer') ?>"/>
+		</div>
+	</div>
+<?php } ?>
 	<button type="submit" class="btn btn-primary mb-2"><?= $tl->get('submit', 'html') ?></button>
 </form>
-<?php } ?>
